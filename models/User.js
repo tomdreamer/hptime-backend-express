050f0a0b84fc52e3089db6a620c001028538c900
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2019-02-28 09:03:57 +0100

    error in model

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema(
   {
     // actually the user email..
     username: {
-      String,
+      type: String,
       required: true,
       unique: true,
       match: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
