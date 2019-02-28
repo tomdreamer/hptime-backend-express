09d16554f1c8c27be714ca24e49659ef1528ef3d
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2019-02-28 11:53:35 +0100

    misfire

--- routes/hospital.js
+++ routes/hospital.js
@@ -3,20 +3,26 @@ const express = require("express");
 const Hospital = require("../models/hospital.js");
 
 const router = express.Router();
-
-router.get("/hospitals", (req, res, next) => {
-  Hospital.find()
-    .sort({ createdAt: -1 })
-    .limit(20)
-    // send the DB query results array as a JSON response to the client
-    .then(phoneResult => res.json(phoneResult))
+router.get("/hospital/:hospitalIdentifier([0-9]+)", (req, res, next) => {
+  const { hospitalIdentifier } = req.params;
+  Hospital.findOne({ identifier: hospitalIdentifier })
+    //  .select("latitude longitude")
+    .then(result => res.json(result))
     .catch(err => next(err));
 });
 
-router.get("/hospitals/:hospitalId", (req, res, next) => {
-  const { hospitalId } = req.params;
-  Hospital.findById(hospitalId)
-    .then(hospitalDoc => res.json(hospitalDoc))
+router.get("/hospitals", (req, res, next) => {
+  let { offset = 0, size = 5 } = req.query;
+
+  if (size > 200) {
+    size = 200;
+  }
+
+  Hospital.find()
+    .sort({ createdAt: -1 })
+    .limit(parseInt(size))
+    .skip(parseInt(offset))
+    .then(result => res.json(result))
     .catch(err => next(err));
 });
 
