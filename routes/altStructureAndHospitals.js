c8301929cd6301a6bbca8ed74fd791fc6e39e860
Samuel Bouaroua <bouaroua.samuel@gmail.com>
2019-03-07 20:51:52 +0100

    alt structure

--- routes/altStructureAndHospitals.js
+++ routes/altStructureAndHospitals.js
@@ -1,27 +1,24 @@
 const express = require("express");
-const AlternativeFacility = require("../models/AlternativeFacility");
-const Hospital = require("../models/hospital");
+//const AlternativeFacility = require("../models/AlternativeFacility");
+//const Hospital = require("../models/hospital");
 const router = express.Router();
 
 router.get("/structure-details/:structureId", (req, res, next) => {
   const { structureId } = req.params;
-  console.log(structureId)
+  console.log(structureId);
   AlternativeFacility.findById(structureId)
     // send the DB query result document as a JSON response to the client
-    .then(structureDoc =>
-      
-      {console.log(structureDoc)
-        if(!structureDoc){
-          Hospital.findById(structureId)
-          .then(structureDoc => res.json(structureDoc))
-        }else{
+    .then(structureDoc => {
+      console.log(structureDoc);
+      if (!structureDoc) {
+        Hospital.findById(structureId).then(structureDoc =>
           res.json(structureDoc)
-        }}) 
+        );
+      } else {
+        res.json(structureDoc);
+      }
+    })
     .catch(err => next(err));
 });
-
-
-
-
 
 module.exports = router;
